5a0fca970c31b9f5d526a1299a1f36a609cafea3
PKB <pargb0553@naver.com>
2026-05-06 09:26:47 +0900

    26/05/06/JAP프로젝트 생성

---
+++ MessageSystem_JPA/src/main/java/com/lg/myapp/MessageSystemJpaApplication.java
@@ -0,0 +1,13 @@
+package com.lg.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSystemJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageSystemJpaApplication.class, args);
+	}
+
+}
